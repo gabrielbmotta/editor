e2b33d81f38a7e416b4a4c5eb29fb47797de92ca
gabrielbmotta <gabrielbenmotta@gmail.com>
2022-09-15 16:09:06 -0400

    Input timeout. Error handling. Code syntax and formatting.

--- main.c
+++ main.c
@@ -1,19 +1,41 @@
+// Gabriel Motta - 2022
+//
+// Based on tutorial at https://viewsourcecode.org/snaptoken/kilo/index.html
+// Implementation of antirez's kilo, a text editor in 1000 lines.
+
+
+//== Includes ==//
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+//=== Data ===//
+
 struct termios orig_termios;
+
+//=== Terminal ===//
+
+void die(const char* s){
+  perror(s);
+  exit(1);
+}
 
 void disableRawMode() 
 {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);  
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+    die("tcsetattr");
+  }
 }
 
 void enableRawMode() 
 {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1){
+    die("tcgetattr");
+  }
+
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
@@ -22,19 +44,32 @@ void enableRawMode()
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){
+    die("tcsetattr");
+  }
 }
+
+//=== Main ===//
 
 int main()
 {
   enableRawMode();
 
-  char c;
-  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+  while(1){
+    char c = '\0';
+    if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN){
+      die("read");    
+    }
     if(iscntrl(c)){
       printf("%d\r\n", c);
     }else{
       printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q'){
+      break;
     }
   }
 
