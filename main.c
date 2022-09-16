f2f63b6a3da9f1d0bf32b83d7290570495d25630
gabrielbmotta <gabrielbenmotta@gmail.com>
2022-09-16 10:02:55 -0400

    Moving cursor with WASD and arrows

--- main.c
+++ main.c
@@ -84,7 +84,25 @@ char editorReadKey()
       die("read");
     }
   }
-  return c;
+
+  if(c == '\x1b'){
+    char seq[3];
+
+    if(read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+    if(seq[0] == '['){
+      switch(seq[1]){
+        case 'A': return 'w';
+        case 'B': return 's';
+        case 'C': return 'd';
+        case 'D': return 'a';
+      }
+    }
+    return '\x1b';
+  } else {
+    return c;
+  }
 }
 
 int getCursorPosition(int *rows, int *cols) 
@@ -92,29 +110,20 @@ int getCursorPosition(int *rows, int *cols)
   char buf[32];
   unsigned int i = 0;
 
-  if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4){
-    return -1;
-  }  
+  if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
   while(i < sizeof(buf) - 1){
-    if(read(STDIN_FILENO, &buf[i], 1) != 1){
-      break;
-    }
-    if(buf[i] == 'R'){
-      break;
-    }
+    if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if(buf[i] == 'R') break;
     i++;
   }
   buf[i] = '\0';
 
   //printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
 
-  if(buf[0] != '\x1b' || buf[1] != '['){
-    return -1;
-  }
+  if(buf[0] != '\x1b' || buf[1] != '[') return -1;
 
-  if(sscanf(&buf[2], "%d;%d", rows, cols) != 2){
-    return -1;
-  }
+  if(sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
 
   return 0;
 }
@@ -124,9 +133,7 @@ int getWindowSize(int* rows, int* cols)
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12){
-      return -1;
-    }
+    if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
     return getCursorPosition(rows, cols);
   }else{
     *cols = ws.ws_col;
@@ -147,10 +154,8 @@ struct abuf {
 void abAppend(struct abuf *ab, const char* s, int len){
   char* new = realloc(ab->b, ab->len + len);
 
-  if(new == NULL){
-    return; 
-  }
-
+  if(new == NULL) return; 
+  
   memcpy(&new[ab->len], s, len);
   ab->b = new;
   ab->len += len;
@@ -214,6 +219,24 @@ void editorRefreshScreen()
 
 //=== Input ===//
 
+void editorMoveCursor(char key)
+{
+  switch (key){
+    case 'a':
+      E.cx--;
+      break;
+    case 'd':
+      E.cx++;
+      break;
+    case 'w':
+      E.cy--;
+      break;
+    case 's':
+      E.cy++;
+      break;
+  }
+}
+
 void editorProcessKeypress()
 {
   char c = editorReadKey();
@@ -222,6 +245,13 @@ void editorProcessKeypress()
       clearScreen();
       exit(0);
       break;
+    case 'w': 
+    case 'a':
+    case 's':
+    case 'd':
+      editorMoveCursor(c);
+      break;
+    
     default:
       break;
   }
