32ef171bdb046ae7043cc9bd42c77529effccbce
gabrielbmotta <gabrielbenmotta@gmail.com>
2022-09-16 14:43:53 -0400

    wrap lines

--- main.c
+++ main.c
@@ -332,14 +332,24 @@ void editorRefreshScreen()
 
 void editorMoveCursor(int key)
 {
+  erow* row = (E.cy >= E.num_rows) ? NULL : &E.row[E.cy];
+    
   switch (key){
     case ARROW_LEFT:
       if(E.cx != 0){
         E.cx--;
+      }else if(E.cy > 0){
+        E.cy--;
+        E.cx = E.row[E.cy].size;
       }
       break;
     case ARROW_RIGHT:
-    E.cx++;
+      if(row && E.cx < row->size){
+        E.cx++;
+      } else if(row && E.cy != E.num_rows - 1){
+        E.cy++;
+        E.cx = 0;
+      }
       break;
     case ARROW_UP:
       if(E.cy != 0){
@@ -351,6 +361,12 @@ void editorMoveCursor(int key)
         E.cy++;
       }
       break;
+  }
+
+  row = (E.cy >= E.num_rows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen){
+    E.cx = rowlen;
   }
 }
 
