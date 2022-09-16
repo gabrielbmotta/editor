b699de25a90bedb6f3f377dffa9ceb795391a033
gabrielbmotta <gabrielbenmotta@gmail.com>
2022-09-16 09:47:58 -0400

    Getting window size and cursor position. Drawing border.

--- main.c
+++ main.c
@@ -9,40 +9,59 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 //=== Defines ===//
 
+#define VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 //=== Data ===//
 
-struct termios orig_termios;
+struct editorConfig{
+  int cx, cy;
+  int screen_rows;
+  int screen_cols;
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 //=== Terminal ===//
 
+void clearScreen()
+{
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 void die(const char* s){
+  clearScreen();
+
   perror(s);
   exit(1);
 }
 
 void disableRawMode() 
 {
-  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1){
     die("tcsetattr");
   }
 }
 
 void enableRawMode() 
 {
-  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1){
+  if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1){
     die("tcgetattr");
   }
 
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK| ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
@@ -68,12 +87,129 @@ char editorReadKey()
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) 
+{
+  char buf[32];
+  unsigned int i = 0;
+
+  if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4){
+    return -1;
+  }  
+  while(i < sizeof(buf) - 1){
+    if(read(STDIN_FILENO, &buf[i], 1) != 1){
+      break;
+    }
+    if(buf[i] == 'R'){
+      break;
+    }
+    i++;
+  }
+  buf[i] = '\0';
+
+  //printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+
+  if(buf[0] != '\x1b' || buf[1] != '['){
+    return -1;
+  }
+
+  if(sscanf(&buf[2], "%d;%d", rows, cols) != 2){
+    return -1;
+  }
+
+  return 0;
+}
+
+int getWindowSize(int* rows, int* cols)
+{
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12){
+      return -1;
+    }
+    return getCursorPosition(rows, cols);
+  }else{
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
+//=== Append Buffer ===//
+
+struct abuf {
+  char* b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char* s, int len){
+  char* new = realloc(ab->b, ab->len + len);
+
+  if(new == NULL){
+    return; 
+  }
+
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf* ab){
+  free(ab->b);
+}
+
 //=== Output ===//
+
+void editorDrawRows(struct abuf* ab)
+{
+  int y;
+  for (y = 0; y < E.screen_rows; y++){
+    if(y == E.screen_rows / 3){
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome), "Editor version %s", VERSION);
+      if(welcomelen > E.screen_cols){
+        welcomelen = E.screen_cols;
+      }
+      int padding = (E.screen_cols - welcomelen) / 2;
+      if(padding){
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while(padding--) {
+        abAppend(ab, " ", 1);
+      }
+      abAppend(ab, welcome, welcomelen);
+
+    } else {
+      abAppend(ab, "~", 1);
+    }
+    abAppend(ab, "\x1b[K", 3);
+    if(y < E.screen_rows - 1){
+      abAppend(ab, "\r\n", 2);
+    }
+  }
+}
 
 void editorRefreshScreen()
 {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
+
+  abAppend(&ab, "\x1b[?25l", 6); //Hide cursor
+  abAppend(&ab, "\x1b[H", 3); //Move cursor to top left
+
+  editorDrawRows(&ab);
+
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
+  abAppend(&ab, "\x1b[?25h", 6); //Show cursor
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+
+  abFree(&ab);
 }
 
 //=== Input ===//
@@ -83,10 +219,23 @@ void editorProcessKeypress()
   char c = editorReadKey();
   switch (c) {
     case CTRL_KEY('q'):
+      clearScreen();
       exit(0);
       break;
     default:
       break;
+  }
+}
+
+//=== Init ===//
+
+void initEditor()
+{
+  E.cx = 0;
+  E.cy = 0;
+
+  if (getWindowSize(&E.screen_rows, &E.screen_cols) == -1){
+    die("getWindowSize");
   }
 }
 
@@ -95,6 +244,7 @@ void editorProcessKeypress()
 int main()
 {
   enableRawMode();
+  initEditor();
 
   while(1){
     editorRefreshScreen();
